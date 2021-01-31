7aae739f4e7f7d86ff4c4d92243c24f03ee5ed54
Alejandro Hoyos <soyoh1@gmail.com>
2021-01-31 13:28:22 +0100

    Tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,6 +19,13 @@ const TemplateWrapper = ({ children }) => {
           href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         ></link>
+
+        <meta property="og:title" content="Blue Cognac" />
+        <meta property="og:url" content="https://www.bluecognac.com" />
+        <meta
+          property="og:image"
+          content="https://www.bluecognac.com/static/9ce870e40c38b4953f72e59b04aa5aad/724c8/bluecognac–premierreve_cover_optimized.jpg"
+        />
       </Helmet>
       <Navbar />
       <div>{children}</div>
