a122a7aac7508efda5a9edbbc85162847b5bfa85
Alejandro Hoyos <soyoh1@gmail.com>
2021-01-31 19:55:22 +0100

    up

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,20 @@ module.exports = {
         purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow",
+            },
+          },
+        ],
+      },
+    },
     "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 };
