634ea3c112efbf57acde89a183b3afd84c7a8654
Alejandro Hoyos <soyoh1@gmail.com>
2021-01-31 22:23:25 +0100

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,6 +85,13 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["G-R7YSYDTWZW"],
+      },
+    },
     "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 };
