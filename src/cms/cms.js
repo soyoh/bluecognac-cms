8e5a4a0a1bad9fa5e458eccbf681935dbcb3d543
Alejandro Hoyos <soyoh1@gmail.com>
2021-06-26 12:20:46 +0200

    add new page to cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,6 +5,7 @@ import cloudinary from "netlify-cms-media-library-cloudinary";
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import IndexNewPreview from "./preview-templates/IndexNewPreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
@@ -12,3 +13,4 @@ CMS.registerMediaLibrary(cloudinary);
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
+CMS.registerPreviewTemplate("New landing", IndexNewPreview);
